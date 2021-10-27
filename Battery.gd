2f834ddfbc1e578eb466f9d5fd2e28ae8a7243d6
John Stoecker <john.stoecker@clever.com>
2021-10-27 15:38:00 -0700

    inventory

---
+++ Battery.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+var dragging = false
+
+signal dragsignal;
+
+func _ready():
+	connect("dragsignal",self,"_set_drag_pc")
+	
+func _process(delta):
+	if dragging:
+		var mousepos = get_viewport().get_mouse_position()
+		self.position = Vector2(mousepos.x, mousepos.y)
+
+func _set_drag_pc():
+	dragging=!dragging
+
+
+func _input(event):
+	if event is InputEventMouseButton: # TODO: touch as well
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			emit_signal("dragsignal")
+		elif event.button_index == BUTTON_LEFT and !event.pressed:
+			emit_signal("dragsignal")
+	elif event is InputEventScreenTouch:
+		if event.pressed and event.get_index() == 0:
+			self.position = event.get_position()
+		
