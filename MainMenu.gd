88317399bf91308fef27a8a7aaacfca93fcc55ee
John Stoecker <john.stoecker@clever.com>
2021-10-28 12:22:53 -0700

    surprise and delight

--- MainMenu.gd
+++ MainMenu.gd
@@ -3,6 +3,9 @@ extends Node2D
 var can_show: = true
 
 var newgame
+var about
+var blank
+var blank_count = 0
 var exit
 var buddy
 
@@ -13,12 +16,26 @@ func _ready():
 	add_child(buddy)
 	newgame = $Control/VBoxContainer/NewGame
 	newgame.connect("pressed", self, "_on_new_game_pressed")
+	blank = $Control/VBoxContainer/Blank
+	blank.connect("pressed", self, "_on_blank_pressed")
+	about = $Control/VBoxContainer/About
+	about.connect("pressed", self, "_on_about_pressed")
 	exit = $Control/VBoxContainer/Exit
 	exit.connect("pressed", self, "_on_exit_pressed")
 
 func _on_new_game_pressed():
 	buddy.free()
 	get_tree().change_scene("res://GameScene.tscn")
+
+func _on_blank_pressed():
+	blank_count += 1
+	if blank_count == 1:
+		buddy.get_node("Label").text = "This button doesn't really do anything. You have pressed this button 1 time."
+	else:
+		buddy.get_node("Label").text = "This button doesn't really do anything. You have pressed this button " + str(blank_count) + " times."
+
+func _on_about_pressed():
+	buddy.get_node("Label").text = "code: stoecker\ncode: vzhou\nassets: gashley\nlevels: antonio"
 
 func _on_exit_pressed():
 	get_tree().quit()
